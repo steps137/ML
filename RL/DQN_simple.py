5873aedc416ce2646ae731512ac406336439e77e
steps137 <60099980+steps137@users.noreply.github.com>
2022-06-17 16:49:09 +0300

    DQN_simple

--- RL/DQN_simple.py
+++ RL/DQN_simple.py
@@ -1,52 +1,9 @@
-﻿import math
-import time
+﻿from   collections import deque
+import random
 import gym
 import numpy as np
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-
-class MemoryBuffer:
-    """ 
-    Overwrite memory for storing the environment model.
-    This implementation is not the most efficient, but it is needed for experimenting with sorting.
-    """
-    def __init__(self, capacity, n_states, n_actions):
-        """
-        capacity  - memory capacity (maximum number of stored items)
-        n_states  - number of state variables
-        n_actions - number of actions
-        """
-        self.capacity = capacity    # maximum number of stored elements
-        self.count = 0              # how many items have already been saved
-        self.index = 0              # index of element to be inserted
-        self.nS    = n_states
-        self.nA    = n_actions
-        self.memo  = np.zeros( (capacity, self.nS*2 + 3), dtype = np.float32)
-
-    #------------------------------------------------------------------------------------        
-    
-    def add(self, s0, a0, s1, r1, done):
-        """ Add example to memory """
-        index = self.count % self.capacity                
-        self.memo[index] = np.concatenate(( s0, s1, [a0], [r1], [1.-done] ) )
-        self.count += 1
-
-    #------------------------------------------------------------------------------------
-        
-    def samples(self, count):
-        """ Return count of random examples from memory """
-        mem_max = min(self.count, self.capacity)
-        indxs = np.random.choice(mem_max, count, replace=False)
-        sample = self.memo[indxs]
-        s0 = sample[:, 0:           self.nS]
-        s1 = sample[:, self.nS:     2*self.nS]
-        a0 = sample[:, 2*self.nS:   2*self.nS+1]
-        r1 = sample[:, 2*self.nS+1: 2*self.nS+2]
-        dn = sample[:, 2*self.nS+2: 2*self.nS+3]
-        return ( torch.tensor(s0), torch.tensor(a0, dtype = torch.int64), 
-                 torch.tensor(s1), torch.tensor(r1), torch.tensor(dn) )
-#========================================================================================
 
 class DQN:
     """ DQN method for for discrete actions """
@@ -93,7 +50,7 @@ class DQN:
         self.loss      = nn.MSELoss()
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.params['lm'], momentum=0.8)
 
-        self.memo = MemoryBuffer(self.params['capacity'], self.nS, self.nA)
+        self.memo = deque(maxlen=self.params['capacity'])
         self.maxQ = torch.zeros( (self.params['batch'], ),  dtype=torch.float32).to(self.gpu)
         
         self.epsilon     = self.params['eps1']        # start value in epsilon greedy strategy
@@ -132,12 +89,12 @@ class DQN:
             s1 = self.scale (s1)
             a1 = self.policy(s1)
 
-            self.memo.add(s0, a0, s1, r1, float(done and t < ticks) )            
-
+            self.memo.append( (s0, a0, s1, r1, 1 - float(done and t < ticks)) )
+            
             if self.frame % self.params['update'] == 0:
                 self.target.load_state_dict( self.model.state_dict() ) 
 
-            if self.memo.count >= self.params['batch']:    
+            if len(self.memo) >= self.params['batch']:    
                 self.learn_model()                         
 
             rew += r1
@@ -166,15 +123,33 @@ class DQN:
             if  episode % stat == 0:                               
                 mean, std = np.mean(rews[-stat:]), np.std(rews[-stat:])                
                 print(f"{episode:6d} rew: {mean:7.2f} ± {std/stat**0.5:4.2f}")
-            
+
+    #------------------------------------------------------------------------------------
+
+    def get_batch(self, count):
+        """ Get batch """
+        S0 = torch.empty((count, self.nS), dtype=torch.float32)
+        A0 = torch.empty((count, 1),       dtype=torch.int64)
+        S1 = torch.empty((count, self.nS), dtype=torch.float32)
+        R1 = torch.empty((count, 1),       dtype=torch.float32)
+        Dn = torch.empty((count, 1),       dtype=torch.float32)
+        
+        batch = random.sample(self.memo, self.params['batch']) 
+        for i, (s0, a0, s1, r1, dn) in enumerate(batch):
+            S0[i] = torch.tensor(s0, dtype=torch.float32)
+            A0[i] = torch.tensor(a0, dtype=torch.int64)
+            S1[i] = torch.tensor(s1, dtype=torch.float32) 
+            R1[i] = torch.tensor(r1, dtype=torch.float32) 
+            Dn[i] = torch.tensor(dn, dtype=torch.float32)
+
+        return S0.to(self.gpu), A0.to(self.gpu), S1.to(self.gpu), \
+               R1.to(self.gpu), Dn.to(self.gpu)
+
     #------------------------------------------------------------------------------------
 
     def learn_model(self):
         """ Model Training """
-        s0, a0, s1, r1, dn = self.memo.samples(self.params['batch'])
-        s0 = s0.to(self.gpu); a0 = a0.view(-1,1).to(self.gpu)
-        s1 = s1.to(self.gpu); r1 = r1.view(-1,1).to(self.gpu);  dn = dn.view(-1,1).to(self.gpu)
-        
+        s0, a0, s1, r1, dn = self.get_batch(self.params['batch'])
         with torch.no_grad():
             y = self.target(s1).detach()
         self.maxQ, _ = torch.max(y, 1)      # maximum Q values for S1
@@ -190,9 +165,7 @@ class DQN:
         L.backward()                   # calculate gradients
         self.optimizer.step()          # adjusting parameters
         
-
-env_name = "CartPole-v0"
-#env_name = "MountainCar-v0"
+env_name = "MountainCar-v0"
 env = gym.make(env_name)
 
 dqn = DQN( env )
@@ -202,14 +175,14 @@ dqn.params = {
     'gamma'    : 0.99,      # discount factor
     'eps1'     : 1.0,       # initial value epsilon
     'eps2'     : 0.001,     # final value   epsilon
-    'decays'   : 1000,      # number of episodes to decay eps1 - > eps2
-    'update'   : 2000,      # target model update rate (in frames = time steps)             
-    'batch'    : 32,        # batch size for training
-    'capacity' : 10000,     # memory size
-    'hiddens'  : [32,32],   # hidden layers
-    'scale'    : False,     # scale or not observe to [-1...1]
-    'optimizer': 'adam',    # optimizer (sgd, adam)
-    'lm'       : 0.0001,    # learning rate           
+    'decays'   : 500,       # number of episodes to decay eps1 - > eps2
+    'update'   : 100,       # target model update rate (in frames = time steps)             
+    'batch'    : 100,       # batch size for training
+    'capacity' : 100000,    # memory size
+    'hiddens'  : [256,128], # hidden layers
+    'scale'    : True,      # scale or not observe to [-1...1]
+    'optimizer': 'sgd',     # optimizer (sgd, adam)
+    'lm'       : 0.001,     # learning rate           
 }
 
 dqn.init()
